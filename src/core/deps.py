155f9f7cfe5666c1146f6188de1a6c7a7930c76a
Darsh Ayde <darshayde@gmail.com>
2026-01-03 09:41:55 +0000

    Fixed bugs in the core deps.py for admin check

--- src/core/deps.py
+++ src/core/deps.py
@@ -45,7 +45,7 @@ async def get_current_user(
 
 
 def admin_required(current_user: User = Depends(get_current_user)) -> User:
-    if current_user.role != "admin":
+    if current_user.role != "ADMIN":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="The user doesn't have enough privileges",
